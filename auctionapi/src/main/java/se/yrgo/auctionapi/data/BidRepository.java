fd9cae67f55ef547bf8fcf0a360aebd79df47638
joakimgidlund <jockegidlund@gmail.com>
2025-12-16 15:00:25 +0100

    test dependency added

--- auctionapi/src/main/java/se/yrgo/auctionapi/data/BidRepository.java
+++ auctionapi/src/main/java/se/yrgo/auctionapi/data/BidRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface BidRepository extends JpaRepository<Bid, Long> {
     List<Bid> findByAuctionId(Long auctionId);
 
-    @Query("SELECT COALESCE(MAX(b.bid_amount, 0) FROM Bid b WHERE b.auction_id = :auctionId")
+    @Query("SELECT COALESCE(MAX(b.bid_amount, 0)) FROM Bid b WHERE b.auction_id = :auctionId")
     Optional<BigDecimal> findHighestBidAmountByAuctionId(Long auctionId);
 }
